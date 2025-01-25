7672f52ad903b2e5f0818cb706329c5c02c998ac
Mahmood El-Mahalawy <142519933+mxhmoodem@users.noreply.github.com>
2025-01-25 23:33:51 +0000

    Update heart_disease_prediction.py with building a classifier and hyperparameter tuning

--- heart_disease_prediction.py
+++ heart_disease_prediction.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
 
 # Load the dataset
 df = pd.read_csv("heart_disease_prediction.csv")
@@ -23,3 +27,19 @@ df_clean.loc[~heartdisease_mask, "Cholesterol"] = cholesterol_with_heartdisease.
 df_clean = pd.get_dummies(df_clean, drop_first=True)
 X = df_clean.drop(["HeartDisease"], axis=1)
 y = df_clean["HeartDisease"]
+
+# Split dataset into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=417)
+
+# Selected features for training
+features = ["Oldpeak", "Sex_M", "ExerciseAngina_Y", "ST_Slope_Flat", "ST_Slope_Up"]
+
+# Scale features using MinMaxScaler
+scaler = MinMaxScaler()
+X_train_scaled = scaler.fit_transform(X_train[features])
+
+# Hyperparameter Tuning: Define KNN classifier and grid search parameters
+knn = KNeighborsClassifier()
+grid_params = {"n_neighbors": range(1, 20), "metric": ["minkowski", "manhattan"]}
+knn_grid = GridSearchCV(knn, grid_params, scoring='accuracy')
+knn_grid.fit(X_train_scaled, y_train)
